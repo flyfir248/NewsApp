ba7138e3a4927742d70e27b09fb4a2243cf80a97
@johnya0 <anoopjohny2000@gmail.com>
2023-06-11 23:59:08 +0200

    commit

--- app.py
+++ app.py
@@ -13,40 +13,36 @@ def index():
     return render_template('index.html')
 
 # Form submission route
-@app.route('/search', methods=['POST'])
+@app.route('/search', methods=['GET', 'POST'])
 def search():
-    topic = request.form['topic']
-    place = request.form['place']
+    if request.method == 'POST':
+        topic = request.form['topic']
+        place = request.form['place']
 
-    # Make a request to the News API
-    url = 'https://newsapi.org/v2/everything'
-    params = {
-        'q': topic,
-        'apiKey': ''  # Replace with your actual News API key
-    }
-    response = requests.get(url, params=params)
-    data = response.json()
-
-    # Extract place information from news data
-    results = []
-    for article in data['articles']:
-        result = {
-            'title': article.get('title', ''),
-            'description': article.get('description', ''),
-            'url': article.get('url', '')
+        # Make a request to the News API
+        url = 'https://newsapi.org/v2/everything'
+        params = {
+            'q': topic,
+            'apiKey': ''  # Replace with your actual News API key
         }
-        results.append(result)
+        response = requests.get(url, params=params)
+        data = response.json()
 
-    total_results = len(results)
-    per_page = 20
-    total_pages = total_results // per_page + (total_results % per_page > 0)
-    page = int(request.args.get('page', 1))
+        # Extract place information from news data
+        results = []
+        for article in data['articles']:
+            result = {
+                'title': article.get('title', ''),
+                'description': article.get('description', ''),
+                'url': article.get('url', '')
+            }
+            results.append(result)
 
-    start = (page - 1) * per_page
-    end = start + per_page
-    paginated_results = results[start:end]
+        return render_template('result.html', results=results)
 
-    return render_template('result.html', results=paginated_results, page=page, total_pages=total_pages)
+    # Handle GET request
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
